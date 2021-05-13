08a8bb5e250a479e2553b88e5a81c4cdba5de319
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-13 11:59:14 +0200

    prefer debug log level for later

--- .github/workflows/ci/ensure-github-release-for-package.js
+++ .github/workflows/ci/ensure-github-release-for-package.js
@@ -1,5 +1,6 @@
 import { ensureGithubReleaseForPackage } from "@jsenv/github-release-package"
 
 ensureGithubReleaseForPackage({
+  logLevel: "debug",
   projectDirectoryUrl: new URL("../../../", import.meta.url),
 })
